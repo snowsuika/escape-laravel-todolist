e3a7d26f6b2476bdcc896318e23c4d16956f5521
Eric Fang <ree0916tw@gmail.com>
2022-02-10 02:52:05 +0800

    install laravel breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -22,6 +23,15 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        $user = User::create([
+                                 'name'     => 'admin',
+                                 'email'    => 'admin@test.com',
+                                 'password' => Hash::make('password'),
+                             ]);
+        $user->forceFill([
+                             'email_verified_at' => now(),
+                         ])->save();
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/todolist', function () {
+    return view('todolist');
+})->middleware(['auth'])->name('todolist');
+
+require __DIR__ . '/auth.php';

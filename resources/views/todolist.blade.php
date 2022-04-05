775809dbfa9327632716c9745abdf36503abac2b
snow <snowsuika@gmail.com>
2022-04-05 20:25:13 +0800

    modify import change completed style

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -43,7 +43,7 @@
                             <tr>
                                 <td>{{ $task->title }}</td>
                                 <td>{{ $task->description }}</td>
-                                <td class="text-center">{{ $task->completed ? '完成' : '未完成' }}</td>
+                                <td class="text-center {{ $task->completed ? 'text-green-600' : ' text-gray-400' }}">{{ $task->completed ? '完成' : '未完成' }}</td>
                                 <td>
                                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" id="updateStatusBtn"
                                             onclick="updateStatus('{{ $task->id }}', '{{ $task->completed }}')">
